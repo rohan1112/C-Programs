c711ead80ff95e1f6be434a00463acd9079c27b7
rohan-1112 <rohankalaskar1112@gmail.com>
2024-05-11 13:10:49 +0530

    Updated Day 1

--- Day 1/Q9.cpp
+++ Day 1/Q9.cpp
@@ -8,20 +8,16 @@ using namespace std;
 
 int main(){
     int arr[]={10,20,30,40,50};
-    int *ptr=arr;
+    int cnt=5;
+    void printArray(int*,int);
+    printArray(arr,cnt);
+}
 
-    for(int i=0;i<5;i++){
-        cout<<*(ptr+i)<<endl;
-    }
-    cout<<"--------------"<<endl;
-    for(int i=0;i<5;i++){
-        cout<<*(ptr+i)<<endl;
-
-    }
-    cout<<"--------------"<<endl;
-
-    for(int i=0;i<5;i++){
-        cout<<*(arr+i)<<endl;
+void printArray(int *arr,int cnt){
+    for(int i=0;i<cnt;i++){
+        cout<<arr[i]<<" ";
+        // or
+        // cout<<*(arr+i)<<" ";
 
     }
 }
